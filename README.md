7c52d422d9aea162b68911c706f4a095740913e0
TimeMachine <knosikay@gmail.com>
2017-12-13 17:00:18 +0800

    inital -local

---
+++ README.md
@@ -0,0 +1,12 @@
+# CPU_RISC-V
+My simple CPU designed on Basys3 with RISC-V standard.
+
+
+## References
+1.三步教你用Verilog写一个CPU
+[[1]](http://marshal-r.iteye.com/blog/2170100)
+[[2]](http://marshal-r.iteye.com/blog/2170102)
+[[3]](http://marshal-r.iteye.com/blog/2170104)
+
+2.MIPS多周期CPU设计（Verilog）
+[LINK](http://blog.csdn.net/quinze_lee/article/details/51355550)
---
+++ src/CPU.srcs/sources_1/new/cpu_core.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2017/12/13 15:39:40
+// Design Name: 
+// Module Name: cpu_core
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cpu_core(
+
+    );
+    
+endmodule
